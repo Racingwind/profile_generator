fd5dec4cec27c7205e3c23169386bf24a2aa093d
Racingwind <louis.k.hu@gmail.com>
2023-11-18 04:27:26 -0800

    added profile generator code

--- survey.js
+++ survey.js
@@ -9,7 +9,7 @@ rl.question('What is your name? ', (name) => {
   rl.question('What do you like to do on your free time? ', (activity) => {
     rl.question('What is your favorite snack? ', (snack) => {
       rl.question('What is your favorite music genre? ', (music) => {
-        
+        console.log(`${name} likes listening to ${music} while ${activity} and sustain the session with ${snack}.`);
         rl.close();
       });        
     });
